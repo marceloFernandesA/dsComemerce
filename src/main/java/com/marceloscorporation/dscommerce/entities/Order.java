d6847e8ab2a7ad694b7698e1bc441c8899d0b4ec
Marcelo Fernandes <marcelo_fernandes_a@hotmail.com>
2025-05-09 09:33:25 -0300

    OrderItem,OrderItemPk

--- src/main/java/com/marceloscorporation/dscommerce/entities/Order.java
+++ src/main/java/com/marceloscorporation/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.marceloscorporation.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -12,7 +15,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Instant moment;
     private OrderStatus status;
 
@@ -22,6 +25,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
 
     public Order() {
@@ -74,6 +80,14 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
 
--- src/main/java/com/marceloscorporation/dscommerce/entities/Payment.java
+++ src/main/java/com/marceloscorporation/dscommerce/entities/Payment.java
@@ -12,7 +12,7 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Instant moment;
 
     @OneToOne
