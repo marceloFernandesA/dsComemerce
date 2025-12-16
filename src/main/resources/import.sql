bb3c69a4e0e747c33fb9a0a362a1afb3b3e5c2bd
Marcelo Fernandes <marcelo_fernandes_a@hotmail.com>
2025-12-15 22:19:55 -0300

    ajuste seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -71,6 +71,13 @@ INSERT INTO tb_payment (order_id, moment) VALUES (1, TIMESTAMP WITH TIME ZONE '2
 INSERT INTO tb_payment (order_id, moment) VALUES (2, TIMESTAMP WITH TIME ZONE '2022-07-30T11:00:00Z');
 
 
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 
 
 
