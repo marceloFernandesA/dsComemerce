a063d98f58d5d654be4700e669414724d68a2929
Marcelo Fernandes <marcelo_fernandes_a@hotmail.com>
2026-01-03 08:34:26 -0300

    correcao de autenticacao

--- src/main/java/com/marceloscorporation/dscommerce/controllers/OrderController.java
+++ src/main/java/com/marceloscorporation/dscommerce/controllers/OrderController.java
@@ -1,15 +1,9 @@
 package com.marceloscorporation.dscommerce.controllers;
 
 import com.marceloscorporation.dscommerce.dto.OrderDTO;
-import com.marceloscorporation.dscommerce.dto.ProductDto;
-import com.marceloscorporation.dscommerce.dto.ProductMinDto;
 import com.marceloscorporation.dscommerce.services.OrderService;
-import com.marceloscorporation.dscommerce.services.ProductService;
-import com.marceloscorporation.dscommerce.services.execeptions.DataBaseException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -24,14 +18,14 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENT')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
 
-    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @PreAuthorize("hasRole('CLIENT')")
     @PostMapping
     public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO dto){
         dto = service.insert(dto);
--- src/main/java/com/marceloscorporation/dscommerce/controllers/UserController.java
+++ src/main/java/com/marceloscorporation/dscommerce/controllers/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENT')")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDTO> getMe() {
         UserDTO dto = service.getMe();
